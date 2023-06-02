883bc3568efc818520742195ead0f628a7ad556a
张宇衡 <coldezhang@foxmail.com>
2023-06-02 17:40:06 +0800

    优化代码

--- src/main/java/site/deercloud/liteworldedit/Events.java
+++ src/main/java/site/deercloud/liteworldedit/Events.java
@@ -5,8 +5,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener {
-
-
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         LiteWorldEdit.instance.getCache().deletePlayerCache(event.getPlayer());
